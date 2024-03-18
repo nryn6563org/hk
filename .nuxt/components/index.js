b87d2c33856ed774a1cf11d296b447d9cf884fe2
jhLee <77824683+nryn6563org@users.noreply.github.com>
2024-03-18 09:14:43 +0900

    css update

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -3,10 +3,6 @@ export { default as DefaultHeader } from '../../components/DefaultHeader.vue'
 export { default as ModalWrap } from '../../components/ModalWrap.vue'
 export { default as SampleComp } from '../../components/SampleComp.vue'
 export { default as SubHeader } from '../../components/SubHeader.vue'
-export { default as IssueCatchAlarm } from '../../components/IssueCatch/CatchAlarm.vue'
-export { default as IssueCatchContentOne } from '../../components/IssueCatch/ContentOne.vue'
-export { default as IssueCatchQuickIssue } from '../../components/IssueCatch/QuickIssue.vue'
-export { default as IssueCatchTodayIssue } from '../../components/IssueCatch/TodayIssue.vue'
 export { default as SignalCatchTab } from '../../components/SignalCatch/CatchTab.vue'
 export { default as SignalCatchTabNotJoin } from '../../components/SignalCatch/CatchTabNotJoin.vue'
 export { default as SignalCatchContentTwo } from '../../components/SignalCatch/ContentTwo.vue'
@@ -14,6 +10,10 @@ export { default as SignalCatchItemCard } from '../../components/SignalCatch/Ite
 export { default as SignalCatchSearchWrap } from '../../components/SignalCatch/SearchWrap.vue'
 export { default as SignalCatchSettingAlarm } from '../../components/SignalCatch/SettingAlarm.vue'
 export { default as SignalList } from '../../components/SignalCatch/SignalList.vue'
+export { default as IssueCatchAlarm } from '../../components/IssueCatch/CatchAlarm.vue'
+export { default as IssueCatchContentOne } from '../../components/IssueCatch/ContentOne.vue'
+export { default as IssueCatchQuickIssue } from '../../components/IssueCatch/QuickIssue.vue'
+export { default as IssueCatchTodayIssue } from '../../components/IssueCatch/TodayIssue.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
